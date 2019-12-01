9c206c7711a9831be27d1f23e52de23234d47893
Lauri Friberg <30544305+xormor2@users.noreply.github.com>
2019-12-01 13:36:35 +0200

    print the value of pi to the 99th decimal
    This is a basic math constant display, the constant is M_PI from <math.h>. It uses the %.99f format sequence for the function printf(...).

---
+++ c/MPI.c
@@ -0,0 +1,9 @@
+//Creative Commons copyright Lauri K. Friberg 2019 December the 1st.
+//Merry Yuletide! This code is open source. I invented it myself.
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+printf("%.99f\n", M_PI); //print the value of pi to the 99th decimal
+return 0;
+}
