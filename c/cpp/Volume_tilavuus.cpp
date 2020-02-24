e3d14ac58629a5b592fc66d6c4eaee8ca7e71d35
Lauri K. Friberg <30544305+xormor2@users.noreply.github.com>
2020-02-24 10:32:07 +0200

    Add files via upload

---
+++ c/cpp/Volume_tilavuus.cpp
@@ -0,0 +1,12 @@
+//Volume_tilavuus.cpp: Tehnyt:/By: Lauri Friberg (c) 2013-2020.
+#include <iostream>
+using namespace std;
+int main()
+{
+float a;
+cout << "V=a³" << endl;
+cout << "a=";
+cin >> a;
+cout <<"V=a³=" << a << "³" << "=" << a*a*a << endl;
+return 0;
+}
