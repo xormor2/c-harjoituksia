23c6fbc8c5d3bc1801e7859c93834df9caffa667
Lauri K. Friberg <30544305+xormor2@users.noreply.github.com>
2020-04-01 04:02:27 +0300

    Add files via upload

---
+++ c/triangular_number.c
@@ -0,0 +1,24 @@
+
+// C Program to find Triangular Number Series 
+#include <stdio.h> 
+  
+// Function to find triangular number 
+void triangular_series(int n) 
+{ 
+    int i, j = 1, k = 1; 
+  
+    // For each iteration increase j by 1 
+    // and add it into k 
+    for (i = 1; i <= n; i++) { 
+        printf(" %d ", k); 
+        j = j + 1; // Increasing j by 1 
+        k = k + j; // Add value of j into k and update k 
+    } 
+} 
+// Driven Function 
+int main() 
+{ 
+    int n = 5; 
+    triangular_series(n); 
+    return 0; 
+} 
