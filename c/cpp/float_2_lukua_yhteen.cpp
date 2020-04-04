cf334efad7bd88ad9db19096d030fe807c29593a
Lauri K. Friberg <30544305+xormor2@users.noreply.github.com>
2020-04-04 18:28:07 +0300

    This program sums 2 float numbers
    The sum of user-given 2 float numbers in a float format, uses float typed variables a, b and c.

---
+++ c/cpp/float_2_lukua_yhteen.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    float a, b, c;
+    cout << "Anna 1. luku:\n";
+    cin >> a;
+    cout << "Anna 2. luku:\n";
+    cin >> b;
+    c = a + b;
+    cout << "Antamiesi lukujen summa on " << c << "\n";
+    return 0;
+}
