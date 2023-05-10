3cd203bf3d3b442e678f9b13679f36716498f2fe
Lauri "Linux" Friberg <30544305+xormor2@users.noreply.github.com>
2023-05-10 15:34:44 +0300

    Add files via upload

---
+++ c/cpp/io.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+int main(void)
+{
+  using namespace std;
+  string merkkijono;
+  cout << "Mitä vittua? Selitä data! Kirjota se tähä: ";
+  cin >> merkkijono;
+  cout << "Prööt. Ai sää kirjoti datan "
+    << merkkijono
+    << ". Ompas viksuu!"
+    << endl;
+ return 0;
+}
