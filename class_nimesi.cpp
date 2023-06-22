93d9611849c34fa21ec6993c48a30de4fe0a9c6b
Lauri "Linux" Friberg <30544305+xormor2@users.noreply.github.com>
2023-06-22 08:38:37 +0300

    Code copyleft LinuxPetteri 2023
    This program uses the class definition to create a simple class that asks and displays (outputs) the name specified in the input

---
+++ class_nimesi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class nimi {
+private:
+  string nimi;
+
+public:
+int kysynimi()
+  {
+  using namespace std;
+  //string nimi;
+  cout << "Mikä on nimesi? ";
+  cin >> nimi; cout << "No hyvää päivää " << nimi << "!\n";
+  return 0;
+  }
+};
+
+int main()
+{
+nimi kysely;
+return kysely.kysynimi();
+}
