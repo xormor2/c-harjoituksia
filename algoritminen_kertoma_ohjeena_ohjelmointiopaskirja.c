fd9645ad04aa05638fd4b7bf408c499f59aafe24
Lauri "Linux" Friberg <30544305+xormor2@users.noreply.github.com>
2023-06-29 18:23:37 +0300

    Add files via upload

---
+++ algoritminen_kertoma_ohjeena_ohjelmointiopaskirja.c
@@ -0,0 +1,26 @@
+//Algoritmi, laskee kertoman, ohjeena
+//käytetty ohjelmointiopaskirjaa
+//C-ohjelmointikieli, OtaData
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+  if(argc<2) {
+  	 puts("Anna kokonaisluku, lasken kertoman.");
+    puts("./ohjelmanimi 5");    
+    exit(1);
+  }  
+  else if(argc>2) {
+  	 puts("Liian monta parametriä, kokeile ./ohjelmanimi 5");
+  	 exit(1);
+  }  
+  int n=1, arg_n=1, tulos=1;
+  arg_n=atoi(argv[1]);
+  n=arg_n;
+  while(n>1) {
+    tulos=tulos*n;
+    n--;
+  }
+  printf("%d\n", tulos);
+  return 0;
+}
